dd14076167cc4dad64d4ba02ede93b506ca53d93
alfred alderson <doingnothing987@gmail.com>
2024-04-19 11:49:51 +0530

    made other manage.py commands run before runserver command

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,13 +2,15 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """
     Add two numbers together
     """
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """
     Subtract x from y and return value
     """
