dd56026bd3ec2ba09aa80cac7cab18edd59690e8
alfred alderson <doingnothing987@gmail.com>
2024-04-22 11:58:14 +0530

    tried again

--- app/core/models.py
+++ app/core/models.py
@@ -3,8 +3,11 @@ Database models
 """
 
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser,
-BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
 
 class UserManager(BaseUserManager):
@@ -17,7 +20,6 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
 
     def create_superuser(self, email, password):
         """Create save and return a new superuser"""
