88d42c83b91f7657df41109fe19d1d071129f5a0
alfred alderson <doingnothing987@gmail.com>
2024-04-25 20:38:52 +0530

    Work of food part of the api

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -4,8 +4,12 @@ Tests for models
 
 # from contextlib import AbstractContextManager
 # from typing import Any
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -49,3 +53,19 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass@123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.5'),
+            description='Sampel recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
